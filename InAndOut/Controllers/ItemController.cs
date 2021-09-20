34045dc091359235fa7e112c781dedea37e30368
matheus.monteiro <matheus.monteiro@PC07751>
2021-09-20 14:59:39 -0300

    created the button for create a new borrow, and created, controller and view.

--- InAndOut/Controllers/ItemController.cs
+++ InAndOut/Controllers/ItemController.cs
@@ -22,5 +22,10 @@ namespace InAndOut.Controllers
             IEnumerable<Item> objList = _db.Items;
             return View(objList);
         }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
