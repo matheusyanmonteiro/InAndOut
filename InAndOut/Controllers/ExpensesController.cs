fc5c6a1a092408c570c801faee9fde076ced21e5
matheus.monteiro <matheus.monteiro@PC07751>
2021-09-20 17:00:20 -0300

    Created the expenses list in basic format.

---
+++ InAndOut/Controllers/ExpensesController.cs
@@ -0,0 +1,42 @@
+﻿using InAndOut.Data;
+using InAndOut.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Controllers
+{
+    public class ExpensesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ExpensesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Expenses> objList = _db.Expenses;
+            return View(objList);
+        }
+
+        //GET-CREATE    
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST-CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Expenses obj)
+        {
+            _db.Expenses.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("index");
+        }
+    }
+}
---
+++ InAndOut/Models/Expenses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InAndOut.Models
+{
+    public class Expenses
+    {
+        [Key]
+        public int Id { get; set; }
+        [DisplayName("Expense name")]
+        public string ExpensesName { get; set; }
+        public double Amount { get; set; }
+    }
+}
